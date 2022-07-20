f5d00729cbb66a186399327dae57cfdbddf15ff3
samdankudjodji <samdantettey@gmail.com>
2022-07-20 16:23:00 +0100

    added a worker class

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,30 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(context: Context, params: WorkerParameters):
+    CoroutineWorker(context, params){
+
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(context = applicationContext)
+        val repository  = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (exception: HttpException) {
+            Result.retry()
+        }
+    }
+
+
+}
